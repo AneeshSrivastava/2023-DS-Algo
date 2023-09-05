be8ddb4b0c1ccd9f323d954e31b1bb50429c2b8d
Aneesh Srivastava <aneesh.s@safe.security>
2023-09-05 17:07:39 +0530

    Feat: Adding scripts

---
+++ DataStructures/Binary Search/find_pivot_of_an_array.py
@@ -0,0 +1,7 @@
+# Problem source: https://youtu.be/6z2HK4o8qcU?t=102
+
+
+# Problem:
+# Input -> 1. sorted array = [1, 2, 3, 7, 9]
+#          2. Array is rotated by 2 places -> new array = [7, 9, 1, 2, 3]
+# Output: Give the pivot of this array -> We assume that here the answer is 1 array value
