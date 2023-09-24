531ac60de66c889772fa6980be0bd09e0f0d9eae
Aneesh Srivastava <aneesh.s@safe.security>
2023-09-24 19:38:22 +0530

    Print some basic patterns

---
+++ Patterns/patterns.py
@@ -0,0 +1,40 @@
+
+i=1
+n=3
+count=1
+while(i<=n):
+    j=1
+    while(j<=n):
+        print(count, end='\t')
+        count+=1
+        j+=1
+    print('')
+    i+=1
+print('-'*60)
+
+
+n=4
+count = 1
+i=1
+while (i<=n):
+    count=1
+    while(count<=i):
+        print('*', end='\t')
+        count+=1
+    print('')
+    i+=1
+
+print('-'*60)
+
+n=4
+count = 1
+i=1
+while (i<=n):
+    count=1
+    while(count<=i):
+        print(i, end='\t')
+        count+=1
+    print('')
+    i+=1
+
+print('-'*60)
